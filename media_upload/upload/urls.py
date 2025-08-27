e8c8d3fc0a3755914cb65736c905036087070eab
thesongg <starmm012969@163.com>
2025-08-27 20:22:39 +0800

    修改文件名称

--- media_upload/upload/urls.py
+++ media_upload/upload/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .redbook import RedBookUpdateViewSet
-
+from .redbookviewset import RedBookUpdateViewSet
 
 
 router = DefaultRouter()
